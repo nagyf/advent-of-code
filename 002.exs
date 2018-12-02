8cd591e017bd043467ece7f0589c1cbdb4355e37
Ferenc Nagy <ferenc.nagy1@ge.com>
2018-12-02 14:06:03 +0100

    Second day, first and second solution

--- 002.exs
+++ 002.exs
@@ -33,8 +33,67 @@ defmodule S002 do
         end)
     end
 
-    def solve_second_part(_ids) do
+    def solve_second_part(ids) do
+        Enum.map(ids, &String.codepoints/1)
+        |> pairs
+        |> Stream.map(fn {xs, ys} -> {{xs, ys}, lcs(xs, ys)} end)
+        |> Stream.drop_while(fn {{xs, _}, {len, _seq}} -> 
+            abs(length(xs) - len) > 1
+        end)
+        |> Enum.map(fn {_, {_, seq}} -> seq end)
+        |> Enum.take(1)
+        |> hd
+    end
+
+    defp pairs([]), do: []
+    defp pairs([x|xs]) do
+        (for y <- xs, do: {x, y}) ++ pairs(xs)
+    end
+
+    defp indexed(xs) do
+        Enum.zip(1..length(xs), xs)
+    end
+
+    defp lcs(xs, ys) do
+        xsi = indexed(xs)
+        ysi = indexed(ys)
+        # Prepare the C table
+        mx = Stream.cycle([0]) |> Stream.zip(0..length(xsi)) |> Enum.reduce(Map.new(), fn {i,j}, acc -> Map.put(acc, {j,i}, 0) end)
+        my = Stream.cycle([0]) |> Stream.zip(0..length(ysi)) |> Enum.reduce(Map.new(), fn {i,j}, acc-> Map.put(acc, {i,j}, 0) end)
         
+        # Fill the C table
+        c = Enum.reduce(ysi, Map.merge(mx, my), fn y, c -> lcs(xsi, y, c) end)
+
+        {
+            lcs_length(c, length(xsi), length(ysi)),
+            lcs_sequence(c, xsi, ysi)
+        }
+    end
+
+    defp lcs(xs, {j, y}, c) do
+        Enum.reduce(xs, c, fn {i, x}, acc ->
+            cond do
+                x == y -> Map.put(acc, {i,j}, acc[{i-1, j-1}] + 1)
+                x != y -> Map.put(acc, {i,j}, max(acc[{i-1, j}], acc[{i, j-1}]))
+            end
+        end)
+    end
+
+    defp lcs_length(c, size_x, size_y) do
+        c[{size_x, size_y}]
+    end
+
+    defp lcs_sequence(c, xsi, ysi) do
+        lcs_sequence_rec(c, Enum.reverse(xsi), Enum.reverse(ysi)) |> Enum.reverse
+    end
+    defp lcs_sequence_rec(_c, _xs, []), do: []
+    defp lcs_sequence_rec(_c, [], _ys), do: []
+    defp lcs_sequence_rec(c, [{i, x}|xs]=xss, [{j, y}|ys]=yss) do
+        cond do
+            x == y -> [x | lcs_sequence_rec(c, xs, ys)]
+            c[{i, j-1}] > c[{i-1, j}] -> lcs_sequence_rec(c, xss, ys)
+            true -> lcs_sequence_rec(c, xs, yss)
+        end
     end
 end
 
