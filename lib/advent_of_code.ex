1b62e052c36970eb7ad7bf0696221fcabf89200c
Ferenc Nagy <ferenc.nagy1@ge.com>
2018-12-10 10:28:33 +0100

    Day 8

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -1,12 +1,13 @@
 defmodule AdventOfCode do
     def solve() do
-        Common.solve("lib/day1.txt", &Day1.solve/1)
-        Common.solve("lib/day2.txt", &Day2.solve/1)
-        Common.solve("lib/day3.txt", &Day3.solve/1)
-        Common.solve("lib/day4.txt", &Day4.solve/1)
-        Common.solve("lib/day5.txt", &Day5.solve/1)
-        Common.solve("lib/day6.txt", &Day6.solve/1)
-        Common.solve("lib/day7.txt", &Day7.solve/1)
-        Common.solve("lib/day8.txt", &Day8.solve/1)
+        #Common.solve("lib/day1.txt", &Day1.solve/1)
+        #Common.solve("lib/day2.txt", &Day2.solve/1)
+        #Common.solve("lib/day3.txt", &Day3.solve/1)
+        #Common.solve("lib/day4.txt", &Day4.solve/1)
+        #Common.solve("lib/day5.txt", &Day5.solve/1)
+        #Common.solve("lib/day6.txt", &Day6.solve/1)
+        #Common.solve("lib/day7.txt", &Day7.solve/1)
+        #Common.solve("lib/day8.txt", &Day8.solve/1)
+        Common.solve("lib/day10.txt", &Day10.solve/1)
     end
 end
